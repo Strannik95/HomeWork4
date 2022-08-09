8dc635852ee1649f88bb4a758ddcc2084830713d
Manap <Manap1995@mail.ru>
2022-08-09 21:36:16 +0300

    Домашняя работа 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
+       //Задание 1
+        int number = 0;
+        while (number < 10){
+            System.out.print( ++number + " " );
+        }
+        System.out.println();
+        for (; number > 0; number--){
+            System.out.print(number + " ");
+        }
+        System.out.println();
+         // Задание 2
+        int friday = 5;
+        for (int firstFriday = friday;firstFriday <= 31; firstFriday += 7){
+            System.out.println("Сегодня пятница,  " + firstFriday + " число. Необходимо подготовить отчет.");
+        }
+        // Задание 3
+        int currentYear = 2022;
+        int start = currentYear - 200;
+        int end = currentYear + 100;
 
+        for (int i = start; i <= end;i++){
+            if (i % 79 == 0){
+                System.out.println(i);
+            }
+        }
+        }
     }
-}
